4f4ad1c05fad0bd24d5a80d93481e27dfe396663
BBHAWNA <bb.18isht@gmail.com>
2025-06-02 17:34:08 +0530

    updated passport.js

--- backend/auth/passport.js
+++ backend/auth/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "${process.env.BASE_URL}/auth/google/callback",
+      callbackURL: '${process.env.BASE_URL}/auth/google/callback', // Use template literal for dynamic base URL
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
